ce6cff22b9467a14c3b6ba5d2be28d13ff55c6db
alejo <cesar.rivera.g@uniautonoma.edu.co>
2025-02-28 08:01:57 -0500

    agregar archivos

---
+++ app/src/main/java/com/example/ej_clase6/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ej_clase6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.first_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
